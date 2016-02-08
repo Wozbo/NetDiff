c0b3b7d4fc1001c756270cf18bee62d648c53521
Evan Kirsch <ekirsch@allegient.com>
2016-02-08 11:15:01 -0500

    Initial Commit

---
+++ NetDiff.Test/DiffCalculatorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Dynamic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetDiff.Test.TestObjects;
+
+namespace NetDiff.Test
+{
+    [TestClass]
+    public class DiffCalculatorTest
+    {
+        private DiffCalculator _calculator;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _calculator = new DiffCalculator();
+        }
+
+        [TestMethod]
+        public void Diff_ProducesAList()
+        {
+            var result = _calculator.Diff(
+                baseObj: new GenericDynamicObject(), 
+                evaluated: new GenericDynamicObject());
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ NetDiff/DiffCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDiff
+{
+    public class DiffCalculator
+    {
+        private double _tolerance;
+
+        public DiffCalculator(double tolerance=1e-6)
+        {
+            _tolerance = tolerance;
+        }
+
+        public ICollection<DiffedItem> Diff(DynamicObject baseObj, DynamicObject evaluated)
+        {
+            return new List<DiffedItem>();
+        }
+    }
+}
